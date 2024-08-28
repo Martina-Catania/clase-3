42b6b8c0f6a3185262f988949e348c0e0b3064cc
Martina-Catania <tinicatania1111@gmail.com>
2024-08-28 20:40:47 -0300

    test de estilos

--- components/Contador.tsx
+++ components/Contador.tsx
@@ -1,22 +1,45 @@
-import { Text, View, Pressable } from "react-native";
+import { Text, View, Pressable, StyleSheet } from "react-native";
 import { useState } from "react";
 
-export default function Contador() {
-    const [cont, setCont] = useState(0);
-    
-    const increase = () => { setCont(prev => prev + 1);}
-    const decrease = () => { setCont(prev => prev - 1);}
-  
-    return (
-      <View style={{
+const styles = StyleSheet.create({
+    viewStyles: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-      }}
-  >
-        <Text>Contador: {cont}</Text>
-        <Pressable onPress= {increase}><Text>Increase</Text></Pressable>
-        <Pressable onPress= {decrease}><Text>Decrease</Text></Pressable>
+    },
+    textStyles: {
+        color: '#000000',
+        fontSize: 32
+    },
+    increaseStyles: {
+        color: '#ffffff',
+        fontSize: 16,
+        backgroundColor: '#00ff00',
+        borderWidth: 10,
+        borderColor: '#00ff00'
+    },
+    decreaseStyles: {
+        color: '#ffffff',
+        fontSize: 16,
+        backgroundColor: '#ff0000',
+        borderWidth: 10,
+        borderColor: '#ff0000'
+    }
+})
+
+export default function Contador() {
+
+    const [cont, setCont] = useState(0);
+    const increase = () => { setCont(prev => prev + 1);}
+    const decrease = () => { setCont(prev => prev - 1);}
+
+    return (
+      <View style={styles.viewStyles}>
+        <Text style = {styles.textStyles}>Contador: {cont}</Text>
+        <View style={{flexDirection:'row'}}>
+            <Pressable onPress= {increase}><Text style={styles.increaseStyles}>Increase</Text></Pressable>
+            <Pressable onPress= {decrease}><Text style={styles.decreaseStyles}>Decrease</Text></Pressable>
+        </View>
       </View>
     )
   }
