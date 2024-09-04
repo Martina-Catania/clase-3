f0cf56f313adeef5aec85c498238f07360392bfa
Martina-Catania <tinicatania1111@gmail.com>
2024-09-04 18:37:08 -0300

    tarea (perder neuronas es mi hobby favorito)

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,27 @@
-import Contador from "@/components/Contador";
-import { Text, View } from "react-native";
+import {Tarjeta} from "@/components/Tarjeta";
+import { Text, View, Pressable, StyleSheet } from "react-native";
+import { useState } from "react";
 
+const styles = StyleSheet.create({
+  viewStyles: {
+      backgroundColor:'beige',
+      display: 'flex',
+      justifyContent: 'center',
+      height: '100%',
+      paddingHorizontal: 30,
+      gap: 20,
+  },
+
+})
+function testPress(){
+
+}
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Contador></Contador>
+    <View style = {styles.viewStyles}>
+      <Tarjeta label={"1"}></Tarjeta>
+      <Tarjeta label={"2"}></Tarjeta>
+      <Tarjeta label={"3"}></Tarjeta>
     </View>
   );
 }
---
+++ components/Tarjeta.tsx
@@ -0,0 +1,35 @@
+import { Text, View, Pressable, StyleSheet } from "react-native";
+import { JSXElementConstructor, ReactElement, ReactNode, ReactPortal, useState } from "react";
+
+const styles = StyleSheet.create({
+    unpressedStyle: {
+        backgroundColor: 'beige',
+        borderWidth: 1,
+        borderColor:'black',
+        padding: 30,
+        justifyContent: 'center',
+        alignItems: 'center'},
+    pressedStyle: {
+        backgroundColor: 'green',
+        borderWidth: 1,
+        borderColor:'black',
+        padding: 30,
+        justifyContent: 'center',
+        alignItems: 'center'}
+})
+
+export interface props {
+    label: string
+}
+
+export function Tarjeta(props: { label: string }) {
+    let [pressed, changePressed] = useState(false); //tarjeta arranca no apretada
+    const changeState = () => {changePressed((prevState) => !prevState);} 
+
+    return  ( 
+         <Pressable onPress= {changeState} 
+            style ={pressed? (styles.pressedStyle) : (styles.unpressedStyle)}>
+                <Text>Tarjeta {props.label}</Text>
+        </Pressable> 
+    ) 
+  }
